88cf7640826aad9553d79fc0a4b835ebc5689219
IvanLastovko <ivan.lastovko@ericsson.com>
2021-08-27 11:38:40 +0300

    reading SN from CSV; put SN dynamically into SQL query; sorting output in the same order as input;

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 from impala.dbapi import connect
+import csv
+list_of_SN = []
+
+with open('Book1.csv', newline='') as csv_file_read:
+    csv_reader = csv.reader(csv_file_read)
+
+    for row in csv_reader:
+        print(row[0])
+        list_of_SN.append(row[0])
+
+    print(str(tuple(list_of_SN)))
 
 error_code_query = """
 WITH summary AS (
@@ -7,9 +18,9 @@ WITH summary AS (
            p.firstfailedmeasurepointname, 
            p.measureitem_result, 
            ROW_NUMBER() OVER(PARTITION BY p.serialnumber 
-                                 ORDER BY p.measures_test_timestamp ASC) AS rank
+                                 ORDER BY p.measures_test_timestamp DESC) AS rank
       FROM plda.curve_testresults_full_extract p
-      WHERE p.serialnumber in ('X617A12367','X617A12321','BU95435600') 
+      WHERE p.serialnumber in """ + str(tuple(list_of_SN)) + """
       AND testpassed = 'N' AND measurepoint_status = 'Fail'
       AND measurepoint_name = 'Average Absolute Value')
  SELECT serialnumber as SerialNumber, firstfailedmeasurepointname, measureitem_result,
@@ -39,11 +50,24 @@ cur.execute(error_code_query)
 result = cur.fetchall()
 
 # Displays result in terminal
-for data in result:
-    print(data)
+final_result = []
+
+for SN in list_of_SN:
+    found = False
+    for data in result:
+        # print(data)
+        if str(data[0]) == str(SN):
+            print('FOUND: ' + SN)
+            found = True
+            final_result.append([SN, data[3]])
+            break
+    if not found:
+        print('DID NOT FOUND: ' + SN)
+        final_result.append([SN, '-'])
+
 
 # Download data to excel sheet and save it with file name "ErrorCodes.xlsx"
-df = pd.DataFrame(list(result))
+df = pd.DataFrame(final_result)
 writer = pd.ExcelWriter('ErrorCodes.xlsx')
 df.to_excel(writer, sheet_name='ErrorCodes')
 writer.save()
